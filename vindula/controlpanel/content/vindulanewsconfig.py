c2b0ff01d9c9321dd04014d562c203bc5870b57a
cesaraugusto <cesaraugusto@liberiun.com>
2011-12-01 17:56:25 -0200

    Adiciona view de edição do discussion  do plone

--- vindula/controlpanel/content/vindulanewsconfig.py
+++ vindula/controlpanel/content/vindulanewsconfig.py
@@ -9,7 +9,7 @@ class IVindulaNewsConfig(form.Schema):
     """ Vindula News Config """
     
     ativa_conpartilhamento = schema.Bool(
-                title=_(u'label_ativa_conpartilhamento', default=u'Ativar o compartilhamento global no portal'),
-                description=_(u'help_activ_share', default=u'Se selecionado, Ativa a opção de compartilhamento para todos os itens do portal'),
+                title=_(u'label_ativa_conpartilhamento', default=u'Ativar o compartilhamento nas redes sociais'),
+                description=_(u'help_activ_share', default=u'Se selecionado, Ativa a opção de compartilhamento nas redes sociais de todos os itens "Noticia" do portal'),
                 default=False
                 )
