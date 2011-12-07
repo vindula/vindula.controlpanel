705ee1610cc3eb0a4127682463f70b4f56911f05
Rafaela S. Sacconi <rafaelasacconi@liberiun.com>
2011-12-07 16:13:41 -0200

    Add fiel "width_page" in themeconfig.

--- vindula/controlpanel/content/themeconfig.py
+++ vindula/controlpanel/content/themeconfig.py
@@ -1,48 +1,12 @@
 # -*- coding: utf-8 -*-
 from zope import schema
 from zope.interface import Interface
-from zope.interface import implements
-from zope.app.component.hooks import getSite
-from zope.schema.vocabulary import SimpleTerm
-from zope.schema.vocabulary import SimpleVocabulary
-from zope.schema.interfaces import IContextSourceBinder
 from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.app.textfield import RichText
 from z3c.relationfield.schema import RelationChoice
-from Products.CMFCore.utils import getToolByName
-from plone.app.vocabularies.types import BAD_TYPES
 from vindula.controlpanel import MessageFactory as _
+from vindula.controlpanel.vocabularies import ListPortalType
 
-    
-class ListPortalType(object):
-    implements(IContextSourceBinder)
-    
-    def __init__(self):
-        # nome do objeto de configuracao do vindula, deve estar na pasta "control-panel-objects"
-        self.object = object 
-
-    def __call__(self, context):
-        terms = []
-        L = []
-
-        obj = getToolByName(context, 'portal_types')
-        pprop = getToolByName(context, 'portal_properties')
-        self.navProps = pprop.navtree_properties
-        
-        if obj:
-            itens  = [t for t in obj.listContentTypes()
-                        if t not in self.navProps.metaTypesNotToList and
-                           t not in BAD_TYPES]
-            
-            if itens is not None:
-                for item in itens:
-                    #id = item.id
-                    #type = item.Metatype()
-                    #name = item.Title()
-                   
-                    terms.append(SimpleTerm(item, item, _(u'option_category', default=unicode(item))))
-                      
-        return SimpleVocabulary(terms)    
     
 # Interface and schema
 
@@ -76,10 +40,13 @@ class IThemeConfig(Interface):
     
     itens_menu = schema.List(
          title=_(u"Itens do menu"),
-         description=_(u"Selecione os tipos de itens que serão apresentados no menu e no sub-menu"),
+         description=_(u"Selecione os tipos de itens que serão apresentados no menu e no sub-menu."),
          value_type=schema.Choice(source=ListPortalType()),
          required=False,
         )
     
-
-    
+    width_page = schema.Int(
+        title=_(u"Largura do site"),
+        description=_(u"Largura do site em pixels, insira apenas números iteiros. Esta configuração não se aplica a todos os temas."),
+        required=False,
+        )
