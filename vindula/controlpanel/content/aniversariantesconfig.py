dcf28d409064183cfacdf6eb54a5a3ea59450311
Cesar Augusto <cesaraugusto@liberiun.com>
2012-09-19 14:36:12 -0300

    Correção do Ticket #173

--- vindula/controlpanel/content/aniversariantesconfig.py
+++ vindula/controlpanel/content/aniversariantesconfig.py
@@ -11,6 +11,5 @@ class IAniversariantesConfig(form.Schema):
     list_campos_user = schema.Text(title=unicode("Campos da listagem de aniversariantes", 'utf-8'),
                                    description=unicode("Adicione os campos que serão visualizados na listagem de aniversariantes.\
                                                         Os campos devem possuir o mesmo nome do campo do perfil do usuário e seguir o formato: Label | Campo", 'utf-8'),
-                                   default = _(u"[Nome] | [name]\n[Departamento] | [organisational_unit]\n[Data de Aniversário] | [date_birth]"),
+                                   default = _(u"[Nome] | [name]\n[Departamento] | [organisational_unit]\n[Data de Aniversário] | [date_birth]\n [E-mail] | [email]"),
                                    required=False)
-    
