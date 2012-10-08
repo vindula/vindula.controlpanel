646b59240eca4d86bb18777fcfa0d55d1bb64ce1
Denis Raison <denisraison@liberiun.com>
2012-10-08 15:25:10 -0300

    Integraçao do usuário do Vindula com o do LDAP/AD e do Plone

--- vindula/controlpanel/browser/views.py
+++ vindula/controlpanel/browser/views.py
@@ -9,11 +9,12 @@ from vindula.myvindula.user import BaseFunc, BaseStore
 
 from Products.CMFCore.Expression import Expression
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import ManagePortal
 from vindula.myvindula.validation import to_utf8, valida_form
 from zope.app.component.hooks import getSite
 
 from plone.registry.interfaces import IRegistry
-from zope.component import queryUtility
+from zope.component import queryUtility, getUtility, getAdapter, getMultiAdapter
 from plone.app.discussion.interfaces import IDiscussionSettings
 from Products.statusmessages.interfaces import IStatusMessage
 from vindula.controlpanel import MessageFactory as _
@@ -26,8 +27,13 @@ import pickle
 from collective.plonefinder.browser.finder import Finder
 from Acquisition import aq_inner
 
-
 from plone.app.layout.viewlets.interfaces import IBelowContent
+
+#Imports para criar a tela de Criar Usuário do Plone
+from plone.app.users.browser.register import BaseRegistrationForm
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from AccessControl import getSecurityManager
+from zope.formlib import form
 
 class ControlPanelView(grok.View):
     grok.context(INavigationRoot)
@@ -781,6 +787,73 @@ class ShowAllRolesUsersView(grok.View):
     grok.context(Interface)
     grok.require('zope2.View')
     grok.name('usergroup-userprefs-all')
+
+#Criacao do formulário de adicionar usuários do plone customizada
+
+class IAddUserSchema(Interface):
+    pass
+    
+class AddUserForm(BaseRegistrationForm):
+
+    label = _(u'heading_add_user_form', default=u'Add New User')
+    description = u""
+    template = ViewPageTemplateFile('views_templates/newuser_form.pt')
+
+    @property
+    def form_fields(self):
+        defaultFields = super(AddUserForm, self).form_fields
+
+        # The mail_me field needs special handling depending on the
+        # validate_email property and on the correctness of the mail
+        # settings.
+        portal = getUtility(ISiteRoot)
+        ctrlOverview = getMultiAdapter((portal, self.request),
+                                       name='overview-controlpanel')
+        mail_settings_correct = not ctrlOverview.mailhost_warning()
+        if not mail_settings_correct:
+            defaultFields['mail_me'].custom_widget = CantSendMailWidget
+        else:
+            # Make the password fields optional: either specify a
+            # password or mail the user (or both).  The validation
+            # will check that at least one of the options is chosen.
+            defaultFields['password'].field.required = False
+            defaultFields['password_ctl'].field.required = False
+            if portal.getProperty('validate_email', True):
+                defaultFields['mail_me'].field.default = True
+            else:
+                defaultFields['mail_me'].field.default = False
+
+        # Append the manager-focused fields
+        allFields = defaultFields + form.Fields(IAddUserSchema)
+        return allFields
+
+    @form.action(_(u'label_register', default=u'Register'),
+                 validator='validate_registration', name=u'register')
+    def action_join(self, action, data):
+        super(AddUserForm, self).handle_join_success(data)
+        
+        portal_groups = getToolByName(self.context, 'portal_groups')
+        user_id = data['username']
+        is_zope_manager = getSecurityManager().checkPermission(ManagePortal, self.context)
+
+        try:
+            # Add user to the selected group(s)
+            if 'groups' in data.keys():
+                for groupname in data['groups']:
+                    group = portal_groups.getGroupById(groupname)
+                    if 'Manager' in group.getRoles() and not is_zope_manager:
+                        raise Forbidden
+
+                    portal_groups.addPrincipalToGroup(user_id, groupname, self.request)
+        except (AttributeError, ValueError), err:
+            IStatusMessage(self.request).addStatusMessage(err, type="error")
+            return
+
+        IStatusMessage(self.request).addStatusMessage(
+            _(u"User added."), type='info')
+        self.request.response.redirect(
+            self.context.absolute_url() +
+            '/@@usergroup-userprefs?searchstring=' + user_id)
         
         
     
