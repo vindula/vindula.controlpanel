ae6b863207cb861efc638e6805e175b0a639b925
Denis Raison <deeraison@gmail.com>
2014-12-11 14:22:00 -0200

    Ajustes para não procurar mais campos de layout das UOs

--- vindula/controlpanel/skins/vindula_controlpanel_templates/OrgStruct_view.py
+++ vindula/controlpanel/skins/vindula_controlpanel_templates/OrgStruct_view.py
@@ -6,13 +6,14 @@ def getOrgStru(ctx,layout):
     if ctx.portal_type != 'Plone Site' and ctx.portal_type != 'OrganizationalStructure':
         return getOrgStru(ctx.aq_parent,layout)
     elif ctx.portal_type == 'OrganizationalStructure':
-        if layout:
-            if ctx.activ_personalit:
-                return ctx
-            else:
-                return getOrgStru(ctx.aq_parent,layout)
-        else:
-            return ctx
+        # if layout:
+        #     if ctx.activ_personalit:
+        #         return ctx
+        #     else:
+        #         return getOrgStru(ctx.aq_parent,layout)
+        # else:
+        #     return ctx
+        return ctx
     
     return ctx
 
